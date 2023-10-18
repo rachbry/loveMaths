925b77979bc2fed66d96bb9638a556948b56d149
Rachel Cutler <rach_cutler@live.co.uk>
2023-10-18 12:14:35 +0000

    random number generators added for num1 and num2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,31 @@
+// Wait for the DOM to finish loading before running the Game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+         button.addEventListener("click", function() {
+            // 'this' refers to button just clicked
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+         })
+    }
+
+})
+/**
+ * The main game 'loop' called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
+
+    // Creates 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
@@ -19,6 +46,8 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion() {
+    Math.random() * 25;
+
 
 }
 
